a833d58abcb01d4409590021805d68dc0a122d62
vmichailcc <vmichail2022@gmail.com>
2022-05-08 11:54:58 +0300

    func zip

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print('Git Academy')
-print('Git Academy2')
-print('Git Academy main')
+list_a = [0,1,2]
+list_b = ['zero', 'one', 'two']
+list_c = ['cero', 'uno', 'dos']
+list_all = list(zip(list_a, list_b, list_c))
+print(list_all)
+for a,b,c in list_all:
+    print(f'{a} is {b} in English and {c} in Spanish.')
